6e10ca16b93f61a9c45567c610910052807565f4
chiyonn <kud0h.chi4@gmail.com>
2025-05-18 14:59:36 +0900

    update: reformat

--- endpoint/base.go
+++ endpoint/base.go
@@ -9,37 +9,36 @@ import (
 )
 
 type APIEndpoint struct {
-	Path       string
-	Method     string
-	Rate       float64
-	Burst      int
-	RateKey    string
-	Client     *client.Client
-	BuildReq   func() (*http.Request, error)
-	ParseResp  func(*http.Response) (any, error)
+	Path      string
+	Method    string
+	Rate      float64
+	Burst     int
+	RateKey   string
+	Client    *client.Client
+	BuildReq  func() (*http.Request, error)
+	ParseResp func(*http.Response) (any, error)
 }
 
 func (ep *APIEndpoint) Do(ctx context.Context) (any, error) {
-    req, err := ep.BuildReq()
-    if err != nil {
-        return nil, err
-    }
+	req, err := ep.BuildReq()
+	if err != nil {
+		return nil, err
+	}
 
-    resp, err := ep.Client.HttpClient.Do(req)
-    if err != nil {
-        return nil, err
-    }
-    defer resp.Body.Close()
+	resp, err := ep.Client.HttpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
 
-    return ep.ParseResp(resp)
+	return ep.ParseResp(resp)
 }
-
 
 func NewEndpoint(client *client.Client, method string, path string, rate float64, burst int, key string) (*APIEndpoint, error) {
 	endpoint := APIEndpoint{
 		Client: client,
 		Method: method,
-		Path: path,
+		Path:   path,
 	}
 	err := endpoint.Client.RateLimitManager.Register(key, rate, burst)
 	if err != nil {
