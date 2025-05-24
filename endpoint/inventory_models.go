bb3dc3989b1ef438c4e328cf02df88e7dfbea7a9
chiyonn <kud0h.chi4@gmail.com>
2025-05-24 17:15:03 +0900

    update: format

--- endpoint/inventory_models.go
+++ endpoint/inventory_models.go
@@ -8,45 +8,45 @@ import (
 
 type GetInventorySummariesParams struct {
 	Details         *bool      `url:"details"`
-	GranularityType string    `url:"granularityType"`
-	GranularityId   string    `url:"granularityId"`
+	GranularityType string     `url:"granularityType"`
+	GranularityId   string     `url:"granularityId"`
 	StartDateTime   *time.Time `url:"startDateTime"`
 	SellerSkus      *[]string  `url:"sellerSkus"`
 	SellerSku       *string    `url:"sellerSku"`
 	NextToken       *string    `url:"nextToken"`
-	MarketplaceIds  []string  `url:"marketplaceIds"`
+	MarketplaceIds  []string   `url:"marketplaceIds"`
 }
 
 type GetInventorySummariesResponse struct {
 	Payload    *GetInventorySummariesResult `json:"payload"`
 	Pagination *model.Pagination            `json:"pagination"`
-	Errors     []*model.Error 				`json:"errors"`
+	Errors     []*model.Error               `json:"errors"`
 }
 
 type GetInventorySummariesResult struct {
-	Granularity        model.Granularity   `json:"granularity"`
-	InventorySummaries []InventorySummary  `json:"inventorySummaries"`
+	Granularity        model.Granularity  `json:"granularity"`
+	InventorySummaries []InventorySummary `json:"inventorySummaries"`
 }
 
 type InventorySummary struct {
-	ASIN             *string            `json:"asin"`
-	FNSKU            *string            `json:"fnSku"`
-	SellerSKU        *string            `json:"sellerSku"`
-	Condition        *string            `json:"condition"`
-	InventoryDetails *InventoryDetails  `json:"inventoryDetails"`
-	LastUpdatedTime  *time.Time         `json:"lastUpdatedTime"`
-	ProductName      *string            `json:"productName"`
-	TotalQuantity    *int	            `json:"totalQuantity"`
-	Stores           *[]string          `json:"stores"`
+	ASIN             *string           `json:"asin"`
+	FNSKU            *string           `json:"fnSku"`
+	SellerSKU        *string           `json:"sellerSku"`
+	Condition        *string           `json:"condition"`
+	InventoryDetails *InventoryDetails `json:"inventoryDetails"`
+	LastUpdatedTime  *time.Time        `json:"lastUpdatedTime"`
+	ProductName      *string           `json:"productName"`
+	TotalQuantity    *int              `json:"totalQuantity"`
+	Stores           *[]string         `json:"stores"`
 }
 
 type InventoryDetails struct {
-	FulfillableQuantity      *int                 `json:"fulfillableQuantity"`
-	InboundWorkingQuantity   *int                 `json:"inboundWorkingQuantity"`
-	InboundShippedQuantity   *int                 `json:"inboundShippedQuantity"`
-	InboundReceivingQuantity *int                 `json:"inboundReceivingQuantity"`
-	ReservedQuantity         *ReservedQuantity    `json:"reservedQuantity"`
-	ResearchingQuantity      *ResearchingQuantity `json:"researchingQuantity"`
+	FulfillableQuantity      *int                   `json:"fulfillableQuantity"`
+	InboundWorkingQuantity   *int                   `json:"inboundWorkingQuantity"`
+	InboundShippedQuantity   *int                   `json:"inboundShippedQuantity"`
+	InboundReceivingQuantity *int                   `json:"inboundReceivingQuantity"`
+	ReservedQuantity         *ReservedQuantity      `json:"reservedQuantity"`
+	ResearchingQuantity      *ResearchingQuantity   `json:"researchingQuantity"`
 	UnfulfillableQuantity    *UnfulfillableQuantity `json:"unfulfillableQuantity"`
 }
 
@@ -58,7 +58,7 @@ type ReservedQuantity struct {
 }
 
 type ResearchingQuantity struct {
-	TotalResearchingQuantity     *int                     `json:"totalResearchingQuantity"`
+	TotalResearchingQuantity     *int                       `json:"totalResearchingQuantity"`
 	ResearchingQuantityBreakdown []ResearchingQuantityEntry `json:"researchingQuantityBreakdown"`
 }
 
